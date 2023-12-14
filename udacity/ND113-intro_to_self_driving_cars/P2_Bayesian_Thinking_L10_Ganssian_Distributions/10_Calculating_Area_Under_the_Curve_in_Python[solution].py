0ba19952f6124d566559f9aac05d8ec8937a3a7e
WK-MBP <iwake@live.cn>
2023-12-14 20:55:16 +0800

    Add

---
+++ udacity/ND113-intro_to_self_driving_cars/P2_Bayesian_Thinking_L10_Ganssian_Distributions/10_Calculating_Area_Under_the_Curve_in_Python[solution].py
@@ -0,0 +1,5 @@
+#Calculating Area Under the Curve Solution
+from scipy.stats import norm
+
+def gaussian_probability(mean, stdev, x_low, x_high):
+    return norm(loc = mean, scale = stdev).cdf(x_high) - norm(loc = mean, scale = stdev).cdf(x_low)
