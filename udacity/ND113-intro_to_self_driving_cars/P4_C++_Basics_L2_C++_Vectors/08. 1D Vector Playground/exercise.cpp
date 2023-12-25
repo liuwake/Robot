bc3441778b98b833a5e937b95d4b55ee234c425f
WK-MBP <iwake@live.cn>
2023-12-25 17:16:45 +0800

    Solved.

---
+++ udacity/ND113-intro_to_self_driving_cars/P4_C++_Basics_L2_C++_Vectors/08. 1D Vector Playground/exercise.cpp
@@ -0,0 +1,12 @@
+//TODO: Use this as a playground to practice with vectors
+
+
+//TODO:
+// Fill out your program's header. The header should contain any necessary
+// include statements and also function declarations
+
+
+//TODO:
+// Write your main program. Remember that all C++ programs need
+// a main function. The most important part of your program goes
+// inside the main function.
